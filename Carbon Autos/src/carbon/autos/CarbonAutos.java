c598caeacea959d32e00355d7b0e5b2134ba45a8
Mohammad Uzair <mohduzair9@gmail.com>
2018-05-26 13:56:29 +0500

    Updated

---
+++ Carbon Autos/src/carbon/autos/CarbonAutos.java
@@ -0,0 +1,11 @@
+package carbon.autos;
+
+public class CarbonAutos {
+
+    public static void main(String[] args) {
+       
+        UserPanel logfrm = new UserPanel();
+        logfrm.setVisible(true);
+    }
+    
+}
---
+++ Carbon Autos/src/carbon/autos/MainUser.java
@@ -0,0 +1,16 @@
+
+package carbon.autos;
+
+public class MainUser {
+
+    public static String MainUser;
+    
+    public static String getMainUser(){
+        return MainUser;
+    }
+    public void setMainUser(String User){
+        this.MainUser = User;
+    }
+    
+    
+}
